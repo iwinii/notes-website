3bce538a9569f893415d86a4c88f0231221d6089
Iza Kasznicka <eblondik12@gmail.com>
2022-05-24 08:46:51 +0200

    added sign up and login pages with places for user's input, buttons for submitting and logging in, flashing messages if errors within inputs occur or when the sign up was successful, added navbar for choosing sign up, home, login or logout

--- website/src/auth.py
+++ website/src/auth.py
@@ -1,15 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>login</p>"
+    return render_template("login.html")
+
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"
+    return render_template("logout.html")
 
-@auth.route('/sign-up')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>sign up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('first-name')
+        password = request.form.get('password')
+        passwordConfirmation = request.form.get('password-confirmation')
+
+        hasErrors = False
+        if len(email) < 4:
+            hasErrors = True
+            flash('Email must be greater than 3 characters.', category='error')
+        if len(firstName) < 2:
+            hasErrors = True
+            flash('First name must be greater than 1 character.', category='error')
+        if password != passwordConfirmation:
+            hasErrors = True
+            flash('Passwords do not match.', category='error')
+        if len(password) < 7:
+            hasErrors = True
+            flash('Password must be greater than 6 characters.', category='error')
+
+        if hasErrors == False:
+            flash('Account created!', category='success')
+    return render_template("signup.html")
--- website/src/views.py
+++ website/src/views.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 
-
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    a = "Variable content"
+    return render_template("home.html", a=a)
